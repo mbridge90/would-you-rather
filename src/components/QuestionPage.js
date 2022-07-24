f3965bfc8b264979a28d169e9887340cc0eb0cf5
mbridge90 <bridgem1@tcd.ie>
2022-07-24 19:34:43 +0100

    Build out Question page, add state management locally

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,6 +1,7 @@
 import {connect} from "react-redux";
 import { useParams } from "react-router-dom";
 import Nav from "./Nav";
+import {useState} from "react";
 
 const withRouter = (Component) => {
   const ComponentWithRouterProp = (props) => {
@@ -10,14 +11,53 @@ const withRouter = (Component) => {
   return ComponentWithRouterProp
 }
 
-const QuestionPage = ({ id, question, avatarURL}) => {
+const QuestionPage = ({ dispatch, id, question, avatarURL, answer}) => {
+  const [answered, setAnswered] = useState(answer !== null);
+  const [selectedAnswer, setSelectedAnswer] = useState(answer)
+
+  console.log(answered)
+  console.log(selectedAnswer)
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    setAnswered(true);
+  }
+
   return (
       <div>
         <Nav />
         <img src={avatarURL} />
         <h3>Would you rather....</h3>
-        <p>{question.optionOne.text}</p>
-        <p>{question.optionTwo.text}</p>
+        <div>
+          <div className="radio">
+            <label>
+              <input
+                  type="radio"
+                  value="optionOne"
+                  checked={selectedAnswer === "optionOne"}
+                  onChange={()=>setSelectedAnswer("optionOne")}
+              />
+              {question.optionOne.text}
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input
+                  type="radio"
+                  value="optionTwo"
+                  checked={selectedAnswer === "optionTwo"}
+                  onChange={()=>setSelectedAnswer("optionTwo")}
+              />
+              {question.optionOne.text}
+            </label>
+          </div>
+
+          {!answered &&
+            <button className="btn btn-default" onClick={formSubmit}>
+              Submit
+            </button>
+          }
+        </div>
       </div>
   )
 }
@@ -26,12 +66,19 @@ const mapStateToProps = ({ questions, users, authedUser }, props) => {
   const questionId = props.router.params.id;
   const question = questions[questionId];
   const avatarURL = users[question.author].avatarURL
+  let answer = null
+
+  if (questions[questionId].optionOne.votes.includes(authedUser)) {
+    answer = "optionOne"
+  } else if (questions[questionId].optionOne.votes.includes(authedUser)) {
+    answer = "optionTwo"
+  }
+
   return {
     id: questionId,
     question,
     avatarURL ,
-    answer: questions[questionId].optionOne.votes.includes(authedUser) ? "optionOne" :
-        questions[questionId].optionOne.votes.includes(authedUser) ? "optionTwo" : null
+    answer,
   }
 }
 
