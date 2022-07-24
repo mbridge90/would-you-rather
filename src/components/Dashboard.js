0da6fda61327a7fbc3f4c2010e45f81c6d38c9ec
mbridge90 <bridgem1@tcd.ie>
2022-07-24 14:10:41 +0100

    MapstateToProps to sort questions into answered and unanswered

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,10 @@
 import { connect } from 'react-redux';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {handleInitialData} from "../actions/shared";
 
 const Dashboard = (props) => {
+  const[viewingUnanswered, setViewingUnanswered] = useState(true);
+
   useEffect(() => {
         props.dispatch(handleInitialData());
       },
@@ -13,12 +15,26 @@ const Dashboard = (props) => {
   return (
       <div>
         <h3>Questions</h3>
+        <h2>Unanswered Questions</h2>
         <ul>
           {
-            props.questionIds.map((id) => (
-                <li key={id}>
+            props.unAnsweredQuestions.map((question) => (
+                <li key={question.id}>
                   <div>
-                    Question id: {id}
+                    Question id: {question.id}
+                  </div>
+                </li>
+            ))
+          }
+        </ul>
+
+        <h2>Answered Questions</h2>
+        <ul>
+          {
+            props.answeredQuestions.map((question) => (
+                <li key={question.id}>
+                  <div>
+                    Question id: {question.id}
                   </div>
                 </li>
             ))
@@ -28,12 +44,24 @@ const Dashboard = (props) => {
   )
 }
 
-const mapStateToProps = ({ questions }) => (
-  {
-    questionIds: Object.keys(questions).sort(
-        (a, b) => questions[b].timestamp - questions[a].timestamp
-    )
+const mapStateToProps = ({ questions, users, authedUser }) => {
+  const question_ids_by_date = Object.keys(questions).sort(
+      (a, b) => questions[b].timestamp - questions[a].timestamp);
+
+  let answeredQuestions = [];
+  let unAnsweredQuestions = [];
+
+  for (const id of question_ids_by_date) {
+    if (Object.keys(users[authedUser].answers).includes(id)) {
+      answeredQuestions.push(questions[id])
+    } else {
+      unAnsweredQuestions.push(questions[id])
+    }
   }
-);
+  return {
+    answeredQuestions,
+    unAnsweredQuestions,
+  }
+}
 
 export default connect(mapStateToProps)(Dashboard);
